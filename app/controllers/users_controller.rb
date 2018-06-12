39ae12f7ae0cb09ac7dd17dbbade3c90a3ff58ca
seventh1976 <seventh1976@gmail.com>
2018-06-12 11:48:25 +0800

    Generate Users controller and view

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,11 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all
+  end
+
+  def import
+    User.import(params[:file])
+
+    redirect_to root_url, notice: "Activity Data imported!"
+  end
+end
